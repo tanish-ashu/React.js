1787f1b27c0aa56bb5b5771e420df46dd1c3912f
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-08-10 14:12:35 +0530

    Foundational 01

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+/* Simple function returning html */
+import Work from "./Work";
+
+function App() {
+  return (
+    <>
+
+    <h1> This is React Foundation </h1>
+    <Work></Work>
+
+    </>
+  )
+}
+export default App;
---
+++ 01basicreact/src/Work.jsx
@@ -0,0 +1,7 @@
+function Work(){
+  return(
+      <h3> Hii, There your work file is ready</h3>
+  )
+}
+
+export default Work;
