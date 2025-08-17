9393a6ad35e3291b1617f365bd879e8103c52658
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-08-17 14:31:37 +0530

    evaluated expression passing in jsx

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -2,10 +2,14 @@
 import Work from "./Work";
 
 function App() {
+  const username = "React Code";
+  /* the below username used is the evaluted expression means we can write final outcome there
+      and not the expresion which is yet to be evaluted i.e if() then etc.  evalution step should be some here this section 
+  */
   return (
     <>
 
-    <h1> This is React Foundation </h1>
+    <h1> This is React Foundation with {username} </h1>  
     <Work></Work>
 
     </>
