b9c0d3e2d440789bca21ce53660865a1ac4b37cc
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-08-17 23:05:38 +0530

    initial commit

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // propogates the change on UI\
+  // returns array containing two things - variable, method controlling variable
+  let [counter, setCounter] = useState(0);
+
+  // let counter = 0;
+
+  const addValue = ()=>{
+  //counter = counter + 1;
+    setCounter(counter + 1);
+  }
+  const removeValue = () =>{
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1> React Learning</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button 
+      onClick={addValue}>Add value {counter}</button>
+      <br/>
+      <br/>
+      <button
+      onClick = {removeValue} >Remove value {counter}</button>
+      <br/>
+      <br/>
+      footer: {counter}
+    </>
+  )
+}
+
+export default App
