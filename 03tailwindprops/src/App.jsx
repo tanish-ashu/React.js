f93836f16d7a0aa04747588fd22afd842dd30fb9
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-08-21 01:27:21 +0530

    Tailwind and Props / Cards

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
+import './App.css'
 import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "tanishk",
+    age: 21
+  }
+  let newArr= [1,2,3]
+
 
 return (
-  <div className="background-color fill-black bg-black flex flex-col items-center justify-center gap-4 p-8">
-    <h1 className="bg-green-400 text-black p-6 text-2xl rounded">Tailwind test</h1>
-    <h1 className="text-3xl font-bold underline text-red-500">Hello Tailwind!</h1>
-    <div className='flex gap-4'>
-        <Card username="chaiaurcode" btnText="click me" />
-        <Card username="hitesh" />
-      </div>
+    <div className="text-black bg-gray-400">
+      <h1 className='bg-green-200 p-4 text-black'  > Hello Tailwind</h1>
+      <h2> Enlighted</h2>
+
+      <Card username="Tanishk Rajak" btnText = "Click Me!" />
+      <Card username="Aryan Choudhary" btnText = "Visit Me!" />
+
   </div>
 );
 
