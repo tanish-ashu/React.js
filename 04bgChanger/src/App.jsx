3656a7f216b4b375b4c0b63bf2479601fea000eb
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-09-03 01:57:25 +0530

    Working bg changer using hook and tailwind

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,36 +1,31 @@
-import {useState} from 'react'
+import { useState } from 'react'
+import Card from './components/Card'
 
 function App() {
   const [color, setColor] = useState('olive')
 
   return (
     <div 
-      className="w-full h-screen"
+      className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
     >
-      <div className="fixed bottom-12 inset-x-0">
-        <div className="flex flex-wrap justify-center gap-3 px-2">
-          <button 
-            onClick={() => setColor("red")} 
-            className="px-4 py-1 bg-red-500 text-white rounded"
-          >
-            Red
-          </button>
-          <button 
-            onClick={() => setColor("green")} 
-            className="px-4 py-1 bg-green-500 text-white rounded"
-          >
-            Green
-          </button>
-          <button 
-            onClick={() => setColor("blue")} 
-            className="px-4 py-1 bg-blue-500 text-white rounded"
-          >
-            Blue
-          </button>
+      <div className= "fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className= "flex flex-wrap shadow-lg bg-white justify-center gap-3 px-3 py-2 rounded-3xl">
+
+          <button onClick={() => setColor("red")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+                style={{backgroundColor: "Red"}}>Red</button>
+          <button onClick={() => setColor("green")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+                style={{backgroundColor: "Green"}}>Green</button>
+          <button onClick={() => setColor("blue")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+                style={{backgroundColor: "Blue"}}>Blue</button>
+          <button onClick={() => setColor("purple")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+                style={{backgroundColor: "purple"}}>Purple</button>
+          <button onClick={() => setColor("yellow")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+                style={{backgroundColor: "Yellow"}}>Yellow</button>
         </div>
       </div>
     </div>
+
   )
 }
 
---
+++ 04bgChanger/src/components/Card.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Card({userName, btnText="click me"}) {
+  console.log(userName); 
+
+  return (
+        <div className="flex flex-col rounded-xl  p-4 mb-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">{userName} </h1>
+            <h1 className="font-bold font-RubikBold">{btnText}</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>0.01</p>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Card
