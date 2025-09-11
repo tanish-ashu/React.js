2c5248498f1311db92c5094df03e661fa503d0aa
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-09-11 11:03:52 +0530

    using onclick on elements , elements using react,  css using tailwind, and an hook

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -12,15 +12,19 @@ function App() {
       <div className= "fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className= "flex flex-wrap shadow-lg bg-white justify-center gap-3 px-3 py-2 rounded-3xl">
 
-          <button onClick={() => setColor("red")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+          <button onClick={() => setColor("red")}  // on click needs a function , not the value returned from funtion 
+           ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
                 style={{backgroundColor: "Red"}}>Red</button>
-          <button onClick={() => setColor("green")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+          <button onClick={() => setColor("green")} // thus callback is a function , that we are giving to on click
+            ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
                 style={{backgroundColor: "Green"}}>Green</button>
           <button onClick={() => setColor("blue")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
                 style={{backgroundColor: "Blue"}}>Blue</button>
-          <button onClick={() => setColor("purple")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+          <button onClick={() => setColor("purple")} 
+            ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
                 style={{backgroundColor: "purple"}}>Purple</button>
-          <button onClick={() => setColor("yellow")} ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
+          <button onClick={() => setColor("yellow")} 
+            ClassName= "outline-none px-4 py-1  rounded-full text-white shadow-lg"
                 style={{backgroundColor: "Yellow"}}>Yellow</button>
         </div>
       </div>
